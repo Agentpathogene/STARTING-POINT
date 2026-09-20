443c4aafb57f7613d766c153ccecb9916f4873e9
Agentpathogene <aidaninfo56@gmail.com>
2026-09-20 16:21:10 +0000

    Add files via upload

---
+++ exercice1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+
+int main (void)
+
+{
+
+   float notea,noteb,notec,noted,notee;
+   printf("\n");
+   printf("Encodeur scolaire :=============>\n");
+   printf("Entrez votre note sur 20 :>");
+   fscanf(stdin,"%f",&notea);
+   printf("Entrez votre 2eme note sur 20 :>");
+   fscanf(stdin,"%f",&noteb); 
+   printf("Entrez votre 3eme note sur 20 :>");
+   fscanf(stdin,"%f",&notec);
+   printf("Entrez votre 4eme note sur 20 :>");
+   fscanf(stdin,"%f",&noted);
+   printf("Entrez votre 5eme note sur 20 :>");
+   fscanf(stdin,"%f",&notee);
+   printf("\n");
+   printf("PATIENTEZ ON CALCUL VOTRE MOYENNE\n");
+
+   float total = notea + noteb + notec +noted +notee;
+   float moyenne = total / 5;
+   printf("VOTRE MOYENNE SUR 20 EST :> %f\n",moyenne);
+   printf("\n");
+
+
+
+
+    return 0;
+}
